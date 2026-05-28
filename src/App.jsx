e3712d14e1f86e863533f520aa094d18bf74ff5f
Adel <ad.rassoul110@gmail.com>
2026-05-28 17:52:49 +0300

    added an empyt home page and its import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter as Router , Routes , Route }
           from 'react-router-dom'; 
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
