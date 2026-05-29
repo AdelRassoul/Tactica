a4d9d013df43113f863f1294b1caf043c9a65b73
Adel <ad.rassoul110@gmail.com>
2026-05-29 16:40:44 +0300

    added contact html

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,28 @@
 function Contact() {
+  
+  return (
+    <main>
+      <div className="contact-container">
+        <h1>Contact Us</h1>
+        
+        <form action="">
+          
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" required />
 
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required />
 
-  return (<h1>Contact Page</h1>);
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" required></textarea>
+
+          <button type="submit">Send Message</button>
+
+        </form>
+
+      </div>
+    </main>
+  );
 }
+
 export default Contact;
